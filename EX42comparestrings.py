515a6c0ba942f6d12f60a6d602b94f4ae11b2344
Chellaram-p <37235951+Chellaram-p@users.noreply.github.com>
2019-02-05 15:27:48 +0530

    Create EX42comparestrings.py

---
+++ EX42comparestrings.py
@@ -0,0 +1,15 @@
+a = str(input(""))
+b = str(input(""))
+if a == b :
+  print("")
+else:
+    print("")
+a1=len(a)
+b1=len(b)
+print()
+if (a1>b1):
+ print(a)
+elif(a1<b1):
+ print(b)
+else:
+ print(a)
