104f6448e1de177ca91dcce27a4944f4dbac77cf
Chellaram-p <37235951+Chellaram-p@users.noreply.github.com>
2019-02-05 14:48:13 +0530

    Update and rename EX41.py to EX41repetation.py

--- EX41.py
+++
@@ -1,6 +0,0 @@
-a = str(input(""))
-b = int(input(""))
-i = 0
-while i < b:
-  print(a)
-  i += 1
---
+++ EX41repetation.py
@@ -0,0 +1,6 @@
+a = str(input("Enter the string"))
+b = int(input("Enter repetation time"))
+i = 0
+while i < b:
+  print(a)
+  i += 1
